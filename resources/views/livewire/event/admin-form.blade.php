d73a4f6a2b5cc501e902efa8db0505fdd3d106eb
Сергей <1@php-cat.com>
2025-06-05 23:16:40 +0500

    2-05 меропир спортсмены + спонсоры ++

--- resources/views/livewire/event/admin-form.blade.php
+++ resources/views/livewire/event/admin-form.blade.php
@@ -143,16 +143,16 @@
     </div>
     <div class="flex flex-wrap flex-row">
         <div class="bg-red-100 w-1/2">
-            <livewire:event.event-attachment-manager-component :eventId="$id" :type="'document'"/>
+            <livewire:event.event-attachment-manager-component :eventId="$id" :type="'document'" :key="'atachment-'.rand()" />
         </div>
         <div class="bg-blue-100 w-1/2">
-            <livewire:event.event-participiant-manager-component :eventId="$id"/>
+            <livewire:event.event-participiant-manager-component :eventId="$id" :key="'parcipants'"/>
         </div>
         <div class="bg-orange-100 w-1/2">
-            <livewire:event.event-guest-manager-component :eventId="$id"/>
+            <livewire:event.event-guest-manager-component :eventId="$id" :key="guest" />
         </div>
         <div class="bg-green-100 w-1/2">
-            <livewire:event.event-sponsor-manager-component :eventId="$id"/>
+            <livewire:event.event-sponsor-manager-component :eventId="$id" :key="sponsor" />
         </div>
     </div>
 </div>
