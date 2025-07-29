abf5f321c54f477643efc4d027fc77f373b5fd31
Сергей <1@php-cat.com>
2025-07-29 10:23:37 +0500

    ++250729

--- resources/views/livewire/app/footer.blade.php
+++ resources/views/livewire/app/footer.blade.php
@@ -3,15 +3,15 @@
         <div class="flex flex-col space-y-3  ">
             <div class="flex flex-col space-y-3 sm:space-y-0 sm:flex-row">
                 <div class="w-full sm:w-1/2 text-center">
-                    &copy; Все права защищены {{ date('Y') }}
+                    v1.1 &copy; Все права защищены {{ date('Y') }}
 {{--                    <br/>--}}
 {{--                    Тех поддержка: <a--}}
 {{--                        class="text-blue-700 hover:underline"--}}
 {{--                        href="mailto:support@php-cat.com">support@php-cat.com</a>--}}
                 </div>
                 <div class="w-full sm:w-1/2 text-center">Создание сервиса <a href="https://php-cat.com"
-                                                                             class="text-blue-600 hover:underline"
-                                                                             target="_blank">php-cat.com</a></div>
+                 class="text-blue-600 hover:underline"
+                 target="_blank">php-cat.com</a></div>
             </div>
         </div>
     </footer>
